b005531e0dd2a16278902b33f56e33738caa918b
MatthewHeun <matthew.heun@me.com>
2019-06-28 14:12:52 -0400

    Add a clean.sh script.

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Script to clean up auxiliary files on UNIX-based systems like macOS.
+# Run this script first if you need a clean compile of the book.
+
+PAPER=Heun-Semieniuk-rebound
+
+rm $PAPER-concordance.tex
+rm $PAPER.aux
+rm $PAPER.bbl
+rm $PAPER.blg
+rm $PAPER.log
+rm $PAPER.out
+rm $PAPER.pdf
+rm $PAPER.spl
+rm $PAPER.synctex.gz
+rm $PAPER.tex
